0558b4d6419a26e8da1cee259680fedb99ddb1b8
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-23 02:57:25 +0700

    all night

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Logo from '../assets/logo_giant.png'
 import SearchingBar from './SearchingBar'
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
     return (
@@ -12,11 +12,15 @@ function NavBar() {
                     <a href="/"><img className='container mt-3 ms-3 w-75' src={Logo} /></a>
                 </div>
                 <SearchingBar />
-                <div className='nav-bar-user mt-4 me-5 ms-3'>
-                    <a href="#"><FontAwesomeIcon icon={faCartShopping} className='shopping-cart-style me-5' /></a>
+                <div className='nav-bar-user mt-4 me-4 ms-3'>
+                    <a href="#"><FontAwesomeIcon icon={faCartShopping} className='shopping-cart-style me-4' /></a>
                     <a className='user-account-name' href="#">
                         <a href="#"><FontAwesomeIcon className='user-profile-style' icon={faUser}/></a>
-                        <a className='account-name-show me-4'>Account</a>
+                        <a href="#">
+                            <a className='account-name-show me-2 '>Account</a>
+                            <FontAwesomeIcon icon={faAngleDown} className='me-4'/>
+                        </a>
+                        
                     </a>
                 </div>
             </div>
