6c32e6089bf29cd2a8c798f3b16b7ad5a740183c
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-23 00:42:55 +0700

    a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Logo from '../assets/logo_giant.png'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
     return (
@@ -12,10 +12,13 @@ function NavBar() {
                 </div>
                 <div className='searching-bar pt-3 pb-3 w-50'>
                     <a>
-                        <a className='me-3 ms-3'>All categories</a>
+                        <a className='all-cate me-3 ms-4'>
+                            <a style={{color: 'black',}}>All categories</a>
+                            <FontAwesomeIcon icon={faAngleDown}/>
+                        </a>
                         <a>Searching anything</a>
                     </a>
-                    <a className='me-3'><FontAwesomeIcon icon={faSearch}/></a>
+                    <a className='me-3' ><FontAwesomeIcon icon={faSearch}/></a>
                 </div>
                 <a>
                     <a>Shopping</a>
