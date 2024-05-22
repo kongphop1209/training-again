cebb68171eeac7ff72e1afc5dede9dde4e2a48b8
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-22 23:22:25 +0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,22 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Logo from '../assets/logo_giant.png'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
     return (
         <div>
             <a className='nav-bar-con'>
                 <div >
-                    <a className='col-md-6 col-xxl-3' href="/"><img src={Logo} /></a>
+                    <a href="/"><img className='container mt-3 ms-3 w-75' src={Logo} /></a>
                 </div>
-                <a>Searching anything</a>
+                <div>
+                    <a>
+                        <a>Searching anything</a>
+                        <a>Searching anything</a>
+                    </a>
+                    <a><FontAwesomeIcon icon={faSearch}/></a>
+                </div>
                 <a>
                     <a>Shopping</a>
                     <a>Account</a>
