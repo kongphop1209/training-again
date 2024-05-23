8959352a0927c3a6278c1b3b44feab85515909c0
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-23 19:51:36 +0700

    a

--- src/components/WrapperHomePage.jsx
+++ src/components/WrapperHomePage.jsx
@@ -1,30 +1,36 @@
 import { faAppleWhole } from "@fortawesome/free-solid-svg-icons/faAppleWhole"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import '../App.css'
-import { faAngleRight} from "@fortawesome/free-solid-svg-icons"
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 
 
 
 function WrapperHomePage() {
   return (
-    <div className="wrapper-home-page mx-auto my-4">
-      <div>
-        <div className="Row-1 ms-2">
-          <FontAwesomeIcon className="mt-5 ms-5" icon={faAppleWhole} style={{color:'white' ,fontSize:'3vw'}}/>
-          <a className="mt-5 ms-3" style={{fontSize:'1.5vw'}}>iPhone 16 Series</a>
+    <div>
+      <div className="wrapper-home-page mx-auto my-4">
+        <div className="ms-5">
+          <div className="Row-1 ms-2 mt-3">
+            <a ><FontAwesomeIcon className="mt-1 ms-5" icon={faAppleWhole} style={{ color: 'white', fontSize: '3vw' }} /></a>
+            <a className="ms-3" style={{ fontSize: '1.5vw' }}>iPhone 16 Series</a>
+          </div>
+          <div className="ms-2">
+            <h1 className="mt-1 ms-5" style={{ color: 'white', fontSize: '4vw' }}>Up to 10%</h1>
+            <h1 className="ms-5" style={{ color: 'white', fontSize: '4vw' }}>off Voucher</h1>
+          </div>
+          <div className="ms-2 mb-3">
+            <a className="ms-5" href='#' style={{ color: 'white', fontSize: '1.3vw' }}>Shop Now</a>
+            <a className="ms-1"><FontAwesomeIcon icon={faAngleRight} style={{ color: 'white', fontSize: '1.2vw' }} /></a>
+          </div>
         </div>
-        <div className="ms-2" style={{color:'white'}}>
-          <h1 className="mt-3 ms-5">Up to 10%</h1>
-          <h1 className="ms-5">off Voucher</h1>
+        <div className="next-prev-wrapper">
+          <a href="#"><FontAwesomeIcon icon={faAngleLeft} style={{background:'White', padding:'1vw', color:'black', }}/></a>
+          <a href="#"><FontAwesomeIcon icon={faAngleRight} style={{background:'White', padding:'1vw', color:'black', }}/></a>
         </div>
-        <div className="ms-3">
-            <a className="ms-5" href='#' style={{textDecoration:'White',color:'white'}}>Shop Now</a>
-            <a className="ms-2"><FontAwesomeIcon icon={faAngleRight} style={{color:'white'}}/></a>
-        </div>
-        
       </div>
       
     </div>
+
   )
 }
 
